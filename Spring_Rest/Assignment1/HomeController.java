aa93783df72e7acfe48d08f02cb384f279e15005
Atharv Dhirde <72916829+atharvdhirde@users.noreply.github.com>
2022-02-22 13:41:49 +0530

    Create HomeController.java

---
+++ Spring_Rest/Assignment1/HomeController.java
@@ -0,0 +1,13 @@
+package rest.REST.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
