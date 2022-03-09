eb397ed0ad6de6755fac2766b87ace3cc04ffe81
Atharv Dhirde <72916829+atharvdhirde@users.noreply.github.com>
2022-03-09 18:39:34 +0530

    Delete ES6 & TypeScript/Assignment1 directory

--- ES6 & TypeScript/Assignment1/Question4/script4.js
+++
@@ -1,6 +0,0 @@
-let names = ['Tom', 'Ivan', 'Jerry'];
-
-let lengths = names.map(name => name.length);
-    
-const newArray = names.map((str) => ({ name: str, length: str.length }))
-console.log(newArray);
